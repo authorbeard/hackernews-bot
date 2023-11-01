115c57f3f37732c2fada01ab6234678a81a4e9ba
authorbeard <msnolan@gmail.com>
2023-11-01 12:43:17 -0700

    change type to hn_type in Story factory

--- spec/factories/story_factory.rb
+++ spec/factories/story_factory.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :story do 
     title { Faker::Music.album }
     by { Faker::Music::Hiphop.artist }
-    type { "story" }
+    hn_type { "story" }
     posted_at { Faker::Time.between(from: DateTime.now - 30, to: DateTime.now) }
     text { Faker::Hipster.paragraph }
     score { Faker::Number.between(from: 1, to: 100) }
